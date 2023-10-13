95b2b95ad1159a334acd20d35047b06f769a9ce4
Andrew Jarman <26150780+AJarman@users.noreply.github.com>
2023-10-13 10:39:23 +0000

    data catalog now working, changed to scenario and single data source type

--- data_load.py
+++ data_load.py
@@ -2,8 +2,10 @@ from typing import Dict, Any, List, Literal, Optional, Union
 from enum import Enum
 from abc import ABC
 from typing_extensions import Annotated
+
 from pydantic import BaseModel, Field
 import yaml
+import pandas as pd
 
 
 class DataCatalogEntryType(Enum):
@@ -23,34 +25,55 @@ class DataCatalogBase(BaseModel, ABC):
     readme: Optional[str] = None
 
 
-class ScenariosCatalogEntry(DataCatalogBase):
-    source_type: Literal["scenarios"] = "scenarios"
-    scenarios: List["DataCatalogEntry"]
 
 
 class CSVCatalogEntry(DataCatalogBase):
     source_type: Literal["csv"] = "csv"
     csv_file: str
 
+    def to_pandas_dataframe(self, pandas_read_csv_kwargs:Optional[dict]) -> pd.DataFrame:
+        """Loads the csv file into a pandas dataframe
+
+        Returns:
+            pd.DataFrame: the dataframe
+        """
+        if read_csv_kwargs is None:
+            read_csv_kwargs = {}
+        return pd.read_csv(self.csv_file, **read_csv_kwargs)
+
 
 class CSVinZIPCatalogEntry(CSVCatalogEntry):
     source_type: Literal["csv_in_zip"] = "csv_in_zip"
-    zip_url: str
+    csv_file: str
 
 
 class SQLQueryCatalogEntry(DataCatalogBase):
     source_type: Literal["sql_query"] = "sql_query"
-    sqlalchemy_url: str
     query: str
+    database_metadata: Optional[Dict[str, Any]] = None
+
+    def to_pandas_dataframe(self, pandas_read_sql_kwargs:Optional[dict]) -> pd.DataFrame:
+        """Loads the sql query into a pandas dataframe
+
+        Returns:
+            pd.DataFrame: the dataframe
+        """
+        if read_sql_kwargs is None:
+            read_sql_kwargs = {}
+        return pd.read_sql(self.query, **read_sql_kwargs)
+    
 
 # 'Register' these types with pydantic
-DataCatalogEntry = Annotated[Union[ScenariosCatalogEntry, CSVCatalogEntry,CSVinZIPCatalogEntry], Field(discriminator='source_type')]
+DataCatalogEntry = Annotated[Union[CSVCatalogEntry, CSVinZIPCatalogEntry,
+                         SQLQueryCatalogEntry], Field(discriminator='source_type')]
 
+class ScenariosCatalogEntry(DataCatalogBase):
+    scenarios: List[DataCatalogEntry]
 
 class DataCatalog(BaseModel):
     domain: str
-    data_sources: List[DataCatalogEntry]
-    exceptions: list = Field(default_factory=list)
+    single_data_sources: List[DataCatalogEntry]
+    scenario_data_sources: List[ScenariosCatalogEntry]
     notes: Optional[str] = None
 
     @classmethod
@@ -81,8 +104,6 @@ class DataCatalog(BaseModel):
         except Exception as error:
             raise Exception(
                 f"Could not load data catalog at {catalog_path}, please check file is valid") from error
-
-        
 
         cat_instance = cls.model_validate(yaml_dict)
         return cat_instance
