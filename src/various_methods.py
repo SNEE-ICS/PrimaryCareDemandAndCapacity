684aa24170e158710e67ef2cc512e306d62d53c9
Ibrahim Khan <148237791+IbrahimKhan07@users.noreply.github.com>
2024-02-19 17:23:19 +0000

    roll back on dictionary in constants.py

--- src/various_methods.py
+++ src/various_methods.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass
 import math
 from functools import lru_cache
 
-from src.schemas import DataCatalog
 import src.constants as const
 
 
@@ -87,44 +86,3 @@ def month_num_to_cos(month:int)->float:
     if month not in range(1,13):
         raise ValueError('month must be in range 1-12')
     return math.cos(_month_to_angle(month))
-
-
-def get_staff_type():
-    
-    # Loading the GP workforce or staffing level dataset
-    data_catalog = DataCatalog.load_from_yaml("data_catalog.yaml")
-    workforce_data_catalog_entry_22 = data_catalog.single_data_sources[4]
-    gp_workforce_df = workforce_data_catalog_entry_22.load()
-    
-    # Initialising a dictionary object to store staff type
-    staff_type = {}
-    apprentices_type = {}
-    
-    # Iterating over each rows of GP workforce dataframe
-    for index, row in gp_workforce_df.iterrows():
-        key = row["STAFF_ROLE"]
-        value = row["STAFF_GROUP"]
-        
-        if key != 'Apprentices':
-            if key in staff_type:
-                if value not in staff_type[key]:
-                    staff_type[key].append(value)
-                # If the key already exist and has a list with single value, replace the list with single value directly
-                elif len(staff_type[key]) == 1:
-                    staff_type[key] = value
-            else:
-                staff_type[key] = [value]
-               
-        # Separate Dictionary for apprentices        
-        else:
-            if key in apprentices_type:
-                if value not in apprentices_type[key]:
-                    apprentices_type[key].append(value) 
-            else:
-                apprentices_type[key] = [value]
-            
-            
-    # Sorting the dictionary on keys
-    staff_type_sorted = dict(sorted(staff_type.items()))
-                
-    return staff_type_sorted, apprentices_type
