82bccb03b0cb58c79bb8372e3155fb80b3bc41da
Andrew Jarman <26150780+AJarman@users.noreply.github.com>
2024-02-22 18:22:18 +0000

    fixes and iterations

--- src/appointments_models.py
+++ src/appointments_models.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Callable
 from pydantic import Field, BaseModel
 
 from sklearn.pipeline import Pipeline, FunctionTransformer
@@ -12,6 +12,10 @@ import scipy.stats as stats
 
 import src.constants as const
 
+from sklearn import set_config
+
+set_config(transform_output='pandas')
+
 
 DEFAULT_POLY_DEGREE = 2
 DEFAULT_PCA_COMPONENTS = 2
@@ -19,7 +23,7 @@ DEFAULT_PCA_COMPONENTS = 2
 
 LINEAR_REGRESSION_GRID_SEARCH = {
     "linear_regression__fit_intercept": [True, False],
-    "linear_regression__normalize": [True, False],
+    "linear_regression__positive": [True, False],
 }
 POLYNOMIAL_GRID_SEARCH = {
     "polynomial__degree": [2, 3, 4],
@@ -28,12 +32,12 @@ POLYNOMIAL_GRID_SEARCH = {
 }
 
 PCA_GRID_SEARCH = {
-    "preprocess_demo__pca__n_components": [2, 3, 4],
-    "preprocess_demo__pca__svd_solver": ["auto", "full", "arpack", "randomized"],
-    "preprocess_demo__pca__whiten": [True, False],
-    "preprocess_demo__pca__copy": [True, False],
-    "preprocess_demo__pca__iterated_power": [None, "auto", "arpack", "randomized"],
-    "preprocess_demo__pca__tol": [None, 0.0, 0.1, 0.01],
+    "preprocess_scale_pca__pca__n_components": [2, 3, 4],
+    "preprocess_scale_pca__pca__svd_solver": ["auto", "full", "arpack", "randomized"],
+    "preprocess_scale_pca__pca__whiten": [True, False],
+    "preprocess_scale_pca__pca__copy": [True, False],
+    "preprocess_scale_pca__pca__iterated_power": [None, "auto", "arpack", "randomized"],
+    "preprocess_scale_pca__pca__tol": [None, 0.0, 0.1, 0.01],
 }
 
 RIDGE_REGRESSION_GRID_SEARCH = {
@@ -59,16 +63,14 @@ ONEHOT_PCA_LINEAR_GRID_SEARCH = {**PCA_GRID_SEARCH, **LINEAR_REGRESSION_GRID_SEA
 
 ONEHOT_PCA_RIDGE_GRID_SEARCH = {**PCA_GRID_SEARCH, **RIDGE_REGRESSION_GRID_SEARCH}
 
+
+
 REGRESSION_METRICS = [
-    "explained_variance",
-    "max_error",
-    "neg_mean_absolute_error",
-    "neg_mean_squared_error",
-    "neg_root_mean_squared_error",
-    "neg_mean_squared_log_error",
-    "neg_root_mean_squared_log_error",
-    "neg_median_absolute_error",
-    "r2",
+    metrics.max_error,
+    metrics.mean_absolute_error,
+    metrics.mean_squared_error,
+    metrics.median_absolute_error,
+    metrics.r2_score
 ]
 
 
@@ -84,7 +86,9 @@ class ModelConfig(BaseModel):
         arbitrary_types_allowed = True
 
 
-def basic_onehot_pipeline(pipeline_kwargs: Optional[dict] = None) -> Pipeline:
+
+
+def basic_onehot_pipeline() -> Pipeline:
     """
     Creates a basic pipeline for one-hot encoding categorical features and performing linear regression.
 
@@ -97,18 +101,15 @@ def basic_onehot_pipeline(pipeline_kwargs: Optional[dict] = None) -> Pipeline:
     # Define the pipeline
     onehot_pipeline = Pipeline(
         [
-            ("preprocess", ColumnTransformer([("onehot", OneHotEncoder(), ["month"])])),
+            ("preprocess", ColumnTransformer([("onehot", OneHotEncoder(sparse_output=False), ["month"])], remainder="passthrough")),
             ("linear_regression", LinearRegression()),
         ]
     )
-    if pipeline_kwargs:
-        onehot_pipeline.set_params(**pipeline_kwargs)
+    onehot_pipeline.set_output(transform='pandas')
     return onehot_pipeline
 
 
-def onehot_polynomial_linear_pipeline(
-    pipeline_kwargs: Optional[dict] = None,
-) -> Pipeline:
+def onehot_polynomial_linear_pipeline() -> Pipeline:
     """
     Creates a pipeline for one-hot encoding, polynomial feature transformation, and linear regression.
 
@@ -122,19 +123,15 @@ def onehot_polynomial_linear_pipeline(
     # Define the pipeline
     poly_pipeline = Pipeline(
         [
-            ("preprocess", ColumnTransformer([("onehot", OneHotEncoder(), ["month"])])),
+            ("preprocess", ColumnTransformer([("onehot", OneHotEncoder(sparse_output=False), ["month"])], remainder="passthrough")),
             ("polynomial", PolynomialFeatures(degree=DEFAULT_POLY_DEGREE)),
             ("linear_regression", LinearRegression()),
         ]
     )
-    if pipeline_kwargs:
-        poly_pipeline.set_params(**pipeline_kwargs)
     return poly_pipeline
 
 
-def onehot_polynomial_ridge_pipeline(
-    pipeline_kwargs: Optional[dict] = None,
-) -> Pipeline:
+def onehot_polynomial_ridge_pipeline() -> Pipeline:
     """
     Creates a pipeline for one-hot encoding, polynomial feature transformation, and ridge regression.
 
@@ -147,17 +144,15 @@ def onehot_polynomial_ridge_pipeline(
     # Define the pipeline
     poly_pipeline = Pipeline(
         [
-            ("preprocess", ColumnTransformer([("onehot", OneHotEncoder(), ["month"])])),
+            ("preprocess", ColumnTransformer([("onehot", OneHotEncoder(sparse_output=False), ["month"])], remainder="passthrough")),
             ("polynomial", PolynomialFeatures(degree=DEFAULT_POLY_DEGREE)),
             ("ridge", Ridge()),
         ]
     )
-    if pipeline_kwargs:
-        poly_pipeline.set_params(**pipeline_kwargs)
     return poly_pipeline
 
 
-def onehot_pca_linear_pipeline(pipeline_kwargs: Optional[dict] = None) -> Pipeline:
+def onehot_pca_linear_pipeline() -> Pipeline:
     """
     Creates a pipeline for one-hot encoding, PCA dimensionality reduction, and linear regression.
 
@@ -170,29 +165,27 @@ def onehot_pca_linear_pipeline(pipeline_kwargs: Optional[dict] = None) -> Pipeli
     """
     # Define the pipeline
     pca_pipeline = Pipeline(
-        [
+        steps=[
             (
-                "preprocess_month",
-                ColumnTransformer([("onehot", OneHotEncoder(), ["month"])]),
-            ),
-            (
-                "preprocess_demo",
+                "preprocess",
                 ColumnTransformer(
-                    [
-                        ("scale", StandardScaler(), const.GP_LIST_LABELS),
-                        ("pca", PCA(n_components=2), const.GP_LIST_LABELS),
-                    ]
+                    transformers=[
+                        ("onehot", OneHotEncoder(sparse_output=False), ["month"]),
+                        ("scale_pca", Pipeline([
+                            ("scale", StandardScaler()),
+                            ("pca", PCA(n_components=2))
+                        ]), const.GP_LIST_AGE_LABELS),
+                    ],
+                    remainder="passthrough"
                 ),
             ),
             ("linear_regression", LinearRegression()),
         ]
     )
-    if pipeline_kwargs:
-        pca_pipeline.set_params(**pipeline_kwargs)
     return pca_pipeline
 
 
-def onehot_pca_ridge_pipeline(pipeline_kwargs: Optional[dict] = None) -> Pipeline:
+def onehot_pca_ridge_pipeline() -> Pipeline:
     """
     Creates a pipeline for one-hot encoding, PCA dimensionality reduction, and linear regression.
 
@@ -205,25 +198,23 @@ def onehot_pca_ridge_pipeline(pipeline_kwargs: Optional[dict] = None) -> Pipelin
     """
     # Define the pipeline
     pca_pipeline = Pipeline(
-        [
+        steps=[
             (
-                "preprocess_month",
-                ColumnTransformer([("onehot", OneHotEncoder(), ["month"])]),
-            ),
-            (
-                "preprocess_demo",
+                "preprocess",
                 ColumnTransformer(
-                    [
-                        ("scale", StandardScaler(), const.GP_LIST_LABELS),
-                        ("pca", PCA(n_components=2), const.GP_LIST_LABELS),
-                    ]
+                    transformers=[
+                        ("onehot", OneHotEncoder(sparse_output=False), ["month"]),
+                        ("scale_pca", Pipeline([
+                            ("scale", StandardScaler()),
+                            ("pca", PCA(n_components=2))
+                        ]), const.GP_LIST_AGE_LABELS),
+                    ],
+                    remainder="passthrough"
                 ),
             ),
             ("ridge", Ridge()),
         ]
     )
-    if pipeline_kwargs:
-        pca_pipeline.set_params(**pipeline_kwargs)
     return pca_pipeline
 
 
@@ -240,10 +231,7 @@ def baseline_scorer(y_true, y_pred):
     #run all the metrics in the regression_metrics list
     metrics_dict = {}
     for metric in REGRESSION_METRICS:
-        if metric.startswith("neg"):
-            score = -1 * getattr(metrics, metric)(y_true, y_pred)
-        else:
-            score = getattr(metrics, metric)(y_true, y_pred)
-        metrics_dict[metric] = score
+            score = metric(y_true, y_pred)
+            metrics_dict[metric.__name__] = score
     return metrics_dict
     
